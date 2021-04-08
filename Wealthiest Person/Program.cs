5a4e36bb271c1521b1d11610eca1898a5bfd2a86
Joshua Applegarth <APPLEGJ7@my.erau.edu>
2021-04-08 09:14:31 -0500

    Add project files.

---
+++ Wealthiest Person/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Wealthiest_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] accounts = new int[3][]
+            {
+                               new int[3] {100, 200, 300 },
+                               new int[1] {500},
+                               new int[4] {400, 600, 500, 0},
+             };
+            Console.WriteLine(MaximumWealth(accounts)); 
+        }
+        public static int MaximumWealth(int[][] bankRoll)
+        {
+            int richKid = 0;
+
+
+            for (int i = 0; i < bankRoll.Length; i++)
+            {
+                int sum = 0;
+
+
+                for (int j = 0; j < bankRoll[i].Length; j++)
+                {
+                    sum += bankRoll[i][j];
+                }
+
+
+                if (sum > richKid)
+                {
+                    richKid = sum;
+                }
+            }
+            return richKid;
+        }
+    }
+}
